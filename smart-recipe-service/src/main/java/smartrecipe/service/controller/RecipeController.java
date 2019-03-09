58610dd6b391dbbb4818c2d1ecb632a158666554
francois <fjougleux@gmail.com>
2019-03-09 14:02:30 +0100

    Add autodescription serach by Bug fix on save by OCR Add save method

--- smart-recipe-service/src/main/java/smartrecipe/service/controller/RecipeController.java
+++ smart-recipe-service/src/main/java/smartrecipe/service/controller/RecipeController.java
@@ -1,17 +1,16 @@
 package smartrecipe.service.controller;
 
 
-import smartrecipe.service.entity.RecipeEntity;
 import io.swagger.annotations.ApiOperation;
-import smartrecipe.service.ocr.GoogleOCRDetection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import smartrecipe.service.entity.RecipeEntity;
+import smartrecipe.service.ocr.GoogleOCRDetection;
 import smartrecipe.service.repository.RecipeRepository;
-
 
 import java.util.List;
 
@@ -36,6 +35,12 @@ public class RecipeController {
         return recipeRepository.findByDescriptionContainingIgnoreCase(description);
     }
 
+    @GetMapping("/recipesbyautodescription/{description}")
+    @ApiOperation("Find recipes searching by key work in description generated with OCR")
+    List<RecipeEntity> findByAutoDescription(@PathVariable("description") String description) {
+        return recipeRepository.findByAutoDescriptionContainingIgnoreCase(description);
+    }
+
     // @PostMapping("/recipes")
     @RequestMapping(value = "/recipes", method = RequestMethod.POST)
     @ApiOperation("Create a new recipe.")
@@ -51,18 +56,45 @@ public class RecipeController {
     @ApiOperation("Create a new recipe with OCR detection on image.")
     RecipeEntity newRecipeWithOCR(@RequestBody RecipeEntity recipe) {
 
-        GoogleOCRDetection ocrDetection = new GoogleOCRDetection();
-        String description = ocrDetection.detect(recipe.getBinaryDescription());
-        recipe.setDescription(description);
+        if (recipe.getBinaryDescription() != null) {
+            GoogleOCRDetection ocrDetection = new GoogleOCRDetection();
+            String autoDescription = ocrDetection.detect(recipe.getBinaryDescription());
+            recipe.setAutoDescription(autoDescription);
+        }
         RecipeEntity recipeEntity = recipeRepository.save(recipe);
         log.info("Recipe created: " + recipeEntity.toString());
         return recipeEntity;
 
     }
 
+    @RequestMapping(value = "/recipesbyte", method = RequestMethod.POST)
+    @ApiOperation("Create a new recipe with OCR detection on image.")
+    String newRecipeWithOCR(@RequestBody byte[] recipeAsByte) {
+
+        RecipeEntity recipe = new RecipeEntity();
+        recipe.setBinaryDescription(recipeAsByte);
+
+        if (recipeAsByte!= null) {
+            GoogleOCRDetection ocrDetection = new GoogleOCRDetection();
+            String autoDescription = ocrDetection.detect(recipeAsByte);
+            recipe.setAutoDescription(autoDescription);
+        }
+        RecipeEntity recipeEntity= recipeRepository.save(recipe);
+        log.info("Recipe created: " + recipeEntity.toString());
+        return recipeEntity.getAutoDescription();
+
+    }
+
     @RequestMapping(value = "/recipes/{id}")
     public ResponseEntity<RecipeEntity> getRecipeById(@PathVariable("id") Long id) {
         return new ResponseEntity(recipeRepository.findById(id), HttpStatus.OK);
+    }
+
+
+    //@RequestMapping(value = "/recipes/{id}")
+    @DeleteMapping("/recipes/{id}")
+    public void deleteById(@PathVariable("id") Long id) {
+        recipeRepository.deleteById(id);
     }
 
 }
