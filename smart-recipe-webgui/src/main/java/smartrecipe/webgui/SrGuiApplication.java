beee940b5fb89b409fe991c217a3ba5cf4d747e3
francois <fjougleux@gmail.com>
2020-05-23 14:18:15 +0200

    remove HTTP for GUI

--- smart-recipe-webgui/src/main/java/smartrecipe/webgui/SrGuiApplication.java
+++ smart-recipe-webgui/src/main/java/smartrecipe/webgui/SrGuiApplication.java
@@ -1,26 +1,21 @@
 package smartrecipe.webgui;
 
-import org.apache.catalina.connector.Connector;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 public class SrGuiApplication {
-
+/*
     //HTTP port
     @Value("${http.port}")
-    private int httpPort;
+    private int httpPort;*/
 
 
     public static void main(String[] args) {
         SpringApplication.run(SrGuiApplication.class, args);
     }
-
+/*
     // Let's configure additional connector to enable support for both HTTP and HTTPS
     @Bean
     public ServletWebServerFactory servletContainer() {
@@ -38,6 +33,6 @@ public class SrGuiApplication {
         connector.setPort(httpPort);
         return connector;
 
-    }
+    }*/
 }
 
